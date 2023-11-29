cdc3c43d7e57c99c7911723a20c34230add8bc9b
ToasterRepo <danieldelfim_003766@hotmail.com>
2023-11-29 10:40:59 -0100

    publicação da branch, removão de todos os espaços

--- sourceWeb/sourceMobile/RegistoMobile.php
+++ sourceWeb/sourceMobile/RegistoMobile.php
@@ -1,10 +1,8 @@
 <?php
 namespace GoEat;
-
 header("Access-Control-Allow-Origin: no-cors");
 header("Access-Control-Allow-Methods: POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type");
-
 if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
     header("Access-Control-Allow-Origin: *");
     header("Access-Control-Allow-Methods: POST, OPTIONS");
@@ -12,30 +10,22 @@ if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
     http_response_code(200);
     exit;
 }
-
 require '../../vendor/autoload.php';
-
 // Habilita o CORS para permitir solicitações entre diferentes domínios
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
-
 // Método permitido para a solicitação
 header("Access-Control-Allow-Methods: POST");
-
 // Permite cabeçalhos específicos
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
-
 // Inicia a sessão
 session_start();
-
 // Verifica se o método da solicitação é POST
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
     // Obtém os dados do corpo da solicitação
     $data = json_decode(file_get_contents("php://input"));
-
     // Verifica se os campos necessários estão presentes
-    if (
-        empty($data->nome)
+    if (empty($data->nome)
         || empty($data->contribuinte)
         || empty($data->telemovel)
         || empty($data->rua)
@@ -51,59 +41,48 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
         http_response_code(400);
         exit;
     }
-
     // Procura o utilizador pelo e-mail
     $resultados = Utilizador::search([['coluna' => 'email', 'operador' => '=' ,'valor' => $data->email]]);
-
     // Verifica se há exatamente um utilizador com o e-mail fornecido
     if (count($resultados) != 0) {
         echo json_encode(["message" => "Já existe um utilizador com este e-mail"]);
         http_response_code(401);
         exit;
     }
-
     if (!filter_var($data->email, FILTER_VALIDATE_EMAIL)){
         echo json_encode(["message" => "O email tem que ser um endereço válido."]);
         http_response_code(401);
         exit;
     }
-
     if (strlen($data->telemovel) < 9 || strlen($data->telemovel) > 9) {
         echo json_encode(["message" => "Número de telemóvel inválido."]);
         http_response_code(401);
         exit;
     }
-
     if (strlen($data->password) < 8) {
         echo json_encode(["message" => "A palavra-passe tem de ter no mínimo 8 caracteres."]);
         http_response_code(401);
         exit;
     }
-
     if ($data->password != $data->confirmacao){
         echo json_encode(["message" => "A palavra-passe e a confirmação tem que ser iguais."]);
         http_response_code(401);
         exit;
     }
-
     $moradas = Morada::search([
         ['coluna' => 'rua', 'operador' => '=' ,'valor' => $data->rua],
         ['coluna' => 'numPorta', 'operador' => '=' ,'valor' => $data->porta],
         ['coluna' => 'codPostal', 'operador' => '=' ,'valor' => $data->codPostal]]);
-
     if (count($moradas) == 0){
         $morada = new Morada($data->rua, $data->porta, $data->codPostal, $data->localizacao, $data->pais);
         $morada->save();
     }else{
         $morada = $moradas[0];
     }
-
     $cliente = new Cliente($data->nome, $data->contribuinte, $data->telemovel, $morada->getId(), 1);
     $cliente->save();
-
     $utilizador = new Utilizador($data->nome, $data->email, password_hash($data->password, PASSWORD_DEFAULT), 2, 1, $cliente->getId());
     $utilizador->save();
-
     echo json_encode(["message" => "Registo bem-sucedido"]);
     http_response_code(200);
     exit;
--- sourceWeb/sourceMobile/loginValidaMobile.php
+++ sourceWeb/sourceMobile/loginValidaMobile.php
@@ -1,10 +1,8 @@
 <?php
 namespace GoEat;
-
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type");
-
 if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
     header("Access-Control-Allow-Origin: *");
     header("Access-Control-Allow-Methods: POST, OPTIONS");
@@ -12,46 +10,35 @@ if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
     http_response_code(200);
     exit;
 }
-
 require '../../vendor/autoload.php';
-
 // Habilita o CORS para permitir solicitações entre diferentes domínios
 header("Access-Control-Allow-Origin: *");
-header("Content-Type: application/json; charset=UTF-8");
-
+header("Content-Type: application/json; charset=utf-8");
 // Método permitido para a solicitação
 header("Access-Control-Allow-Methods: POST");
-
 // Permite cabeçalhos específicos
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
-
 // Inicia a sessão
 session_start();
-
 // Verifica se o método da solicitação é POST
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
     // Obtém os dados do corpo da solicitação
     $data = json_decode(file_get_contents("php://input"));
-
     // Verifica se os campos necessários estão presentes
     if (empty($data->email) || empty($data->password)) {
         echo json_encode(["message" => "Campos obrigatórios ausentes"]);
         http_response_code(400);
         exit;
     }
-
     // Procura o utilizador pelo e-mail
     $resultados = Utilizador::search([['coluna' => 'email', 'operador' => '=' ,'valor' => $data->email]]);
-
     // Verifica se há exatamente um utilizador com o e-mail fornecido
     if (count($resultados) !== 1) {
         echo json_encode(["message" => "Não existe nenhum utilizador com este e-mail"]);
         http_response_code(401);
         exit;
     }
-
     $utilizador = $resultados[0];
-
     // Verifica se o utilizador está ativo
     if ($utilizador->getAtivo() == 1) {
         // Verifica a correspondência da senha
